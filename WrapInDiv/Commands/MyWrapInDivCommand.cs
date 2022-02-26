6826291fff1c79f6977bb72512ea8deba714289e
Mensur Islami <mi@live.de>
2022-02-26 19:37:00 +0100

    first commit

---
+++ WrapInDiv/Commands/MyWrapInDivCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace WrapInDiv
+{
+    [Command(PackageIds.MyWrapInDivCommand)]
+    internal sealed class MyWrapInDivCommand : BaseCommand<MyWrapInDivCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            var docView = await VS.Documents.GetActiveDocumentViewAsync();
+            var selections = docView?.TextView.Selection.SelectedSpans;
+
+            for (int i = selections.Count - 1; i >= 0; i--)
+            {
+                docView.TextBuffer.Replace(selections[i], $@"<div class="""">{selections[i].GetText()}</div>");
+            }
+
+        }
+    }
+}
